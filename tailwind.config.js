afcbd17d5f970087904064f503c2d11833a2984c
adblanc <ablanc@student.42.fr>
2020-10-28 21:42:06 +0100

    made purge take into account .ts files aswell

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
         removeDeprecatedGapUtilities: true,
         purgeLayersByDefault: true
     },
-    purge: ['./src/*.html'],
+    purge: ['./src/**/*'],
     theme: {
         extend: {}
     },
